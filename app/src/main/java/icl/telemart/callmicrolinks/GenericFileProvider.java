20d43f73bfd0905ca3b2de52a1d646b4aa7b89a8
AzharAsghar <Azharaenaa001500@gmail.com>
2018-09-27 11:52:43 +0500

    Call Recorder App

---
+++ app/src/main/java/icl/telemart/callmicrolinks/GenericFileProvider.java
@@ -0,0 +1,14 @@
+package icl.telemart.callmicrolinks;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by VS00481543 on 14-12-2017.
+ */
+
+public class GenericFileProvider extends FileProvider {
+
+
+    public GenericFileProvider() {
+    }
+}
